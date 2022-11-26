a8b06d48bce489bca75ce5df9b1c6b51161db6fa
Luqman Bolajoko <luksyb@yahoo.com>
2022-11-26 18:43:04 +0100

    try send message

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ client.on("message", async (message) => {
   if (message.body === "Fulus") {
     const pin = (await Math.floor(Math.random() * 100000)) + 1;
     message.reply(`Please enter ${pin} on the app or web page`);
-    const num = message.getContact();
-    console.log((await num).number);
+    const num = await message.getContact();
+    console.log(num.number);
+  }
+});
+
+client.isRegisteredUser("2348030441069@c.us").then(function (isRegistered) {
+  if (isRegistered) {
+    client.sendMessage("2348030441069@c.us", "Fulus server is runing...");
   }
 });
 
