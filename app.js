349536007b6b815ba48bd2a40d1c1ed3cda1c362
Luqman Bolajoko <luksyb@yahoo.com>
2022-11-26 16:29:54 +0100

    fix import

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const qrcode = require("qrcode-terminal");
 
-const { Client } = require("whatsapp-web.js");
+// const { Client } = require("whatsapp-web.js");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 // const client = new Client();
 
 const client = new Client({
